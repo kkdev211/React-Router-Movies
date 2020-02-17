60d81b27fc9245882fd6bf7cadb27f71fbccc2fa
kkdev211 <kkdev211@users.noreply.github.com>
2020-02-16 22:24:39 -0500

    part one

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieCard from './Movies/MovieCard';
+import MovieList from './Movies/MovieList';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -10,9 +14,29 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="App">
+      <nav>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div className="nav-links">
+        <Link to="/">Movie List></Link>
+        <Link to="/movies"> Movie></Link>
+      </div>
+      </nav>
+      <Route path="/" exact component={MovieList} />
+  
+      <Route
+        path="/movies/:id"
+        render={routeProps => {
+          console.log("routeProps", routeProps);
+          return(
+            <Movie
+            match={routeProps.match}
+            history={routeProps.history}
+            location={routeProps.location}
+          />
+          );
+        }}
+      />
     </div>
   );
 };
